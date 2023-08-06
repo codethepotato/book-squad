84a45e893893cff27f947d70e839d162e9466edd
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-08-06 05:43:20 -0500

    starter

--- server/app.py
+++ server/app.py
@@ -1,3 +1,32 @@
 from flask import Flask
+from models import db, Episode, Guest, Appearance
+from flask_migrate import Migrate
+from flask import Flask, request, make_response
+from flask_restful import Api, Resource
+from flask_cors import CORS
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db = SQLAlchemy(metadata=metadata)
+db.init_app(app)
+api = Api(app)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
 
 
--- server/models.py
+++ server/models.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+metadata = MetaData(namingconvention={
+    "fk": "fk%(tablename)s%(column_0name)s%(referred_table_name)s",
+})
 
 
-class Book:
-    pass
