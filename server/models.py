e7ab86e2ab1d50cdebed711047af8ba82835332c
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-08-06 08:24:54 -0500

    fixed

--- server/models.py
+++ server/models.py
@@ -12,6 +12,8 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+db = SQLAlchemy(metadata=metadata)
+
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
