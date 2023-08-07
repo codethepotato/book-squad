bff2c6b6d218b32d0296501c96f9b130f918d9b3
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-08-07 12:16:10 -0500

    order cards

--- client/flatrentals/src/components/Home.js
+++ client/flatrentals/src/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
-import  {useState}  from "react";
+import { useState } from "react";
 import HomeCard from './HomeCard'
 import TopBanner from './TopBanner'
 
-function Home({programmers}){
+function Home({ programmers, selectedOrders, setSelectedOrders }) {
 
     const programmerList = programmers.map(p => {
         return <HomeCard
-        key={p.id}
-        name={p.name}
-        picture={p.picture}
-        specialty={p.specialty} />
+            key={p.id}
+            name={p.name}
+            picture={p.picture}
+            specialty={p.specialty}
+            selectedOrders={selectedOrders}
+            setSelectedOrders={setSelectedOrders} />
     })
-    return(
-        <div className="Homepage"> 
+    return (
+        <div className="Homepage">
             <div>
                 <TopBanner />
                 <div className="ui stackable four column grid">
--- client/flatrentals/src/components/HomeCard.js
+++ client/flatrentals/src/components/HomeCard.js
@@ -2,34 +2,35 @@ import React from 'react';
 import { Card, Icon, Image } from 'semantic-ui-react'
 
 
-function HomeCard({name, specialty, picture,}) {
+function HomeCard({ name, specialty, picture, selectedOrders, setSelectedOrders }) {
 
   function handleOrder() {
+    setSelectedOrders(prevOrders => [...prevOrders, { name, specialty, picture }])
     console.log('was clicked')
   }
 
-    return(
-        <div className="column">
-       <Card>
-    <Image src={picture} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        {specialty}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon onClick={handleOrder} name='user' />
-        Order
-      </a>
-    </Card.Content>
-  </Card>
-  </div>
-    )
+  return (
+    <div className="column">
+      <Card>
+        <Image src={picture} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            {specialty}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon onClick={handleOrder} name='user' />
+            Order
+          </a>
+        </Card.Content>
+      </Card>
+    </div>
+  )
 }
 
 export default HomeCard
