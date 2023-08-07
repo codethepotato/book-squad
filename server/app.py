ff7b8dfdf3d43161fea9f1b9fa9842f30e652730
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-08-07 00:03:03 -0500

    front and back

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@ from models import db, User, Programmer, Order
 from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
+from flask_cors import CORS
+from flask import jsonify
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,6 +12,7 @@ DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -29,9 +32,9 @@ api.add_resource(Users, '/users')
 class Programmers(Resource):
     def get(self):
         programmers = [p.to_dict() for p in Programmer.query.all()]
-        return make_response(programmers, 200)
+        return make_response(jsonify(programmers), 200)
     
-api.add_resource(Programmers, '/programmers')
+api.add_resource(Programmers, '/programmers/')
 
 class Orders(Resource):
     def get(self):
